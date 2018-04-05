789de6d3f0f1a42ba40c2dbf1d130348d4119e0e
johankasperi <johan@kasperi.se>
2018-04-05 19:29:57 +0200

    Remove android and rearrange ios project

--- index.android.js
+++ index.android.js
@@ -1,5 +1,3 @@
-var ReactAppShortcuts = require('react-native').NativeModules.ReactAppShortcuts;
-
 module.exports = {
   /**
    * An initial action will be available if the app was cold-launched
@@ -9,27 +7,21 @@ module.exports = {
    * action object, or `null`. Subsequent invocations will return null.
    */
   popInitialAction: function() {
-    return ReactAppShortcuts.popInitialAction();
+    return null;
   },
 
   /**
    * Adds shortcut items to application
    */
-  setShortcutItems: function(items) {
-    ReactAppShortcuts.setShortcutItems(items);
-  },
+  setShortcutItems: function(items) {},
 
   /**
    * Clears all previously set dynamic icons
    */
-  clearShortcutItems: function() {
-    ReactAppShortcuts.clearShortcutItems();
-  },
+  clearShortcutItems: function() {},
 
   /**
    * Check if quick actions are supported
    */
-   isSupported: function(callback) {
-     ReactAppShortcuts.isSupported(callback);
-   }
+   isSupported: function(callback) {}
 };
